69649a126fbebeb523b9602478277450358fe89c
Владислав Май <91732525+DeLoreannn@users.noreply.github.com>
2022-01-06 20:13:47 +0200

    Add files via upload

---
+++ Task14/c#db/csqlite/DbHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using Microsoft.Data.Sqlite;
+using System.Linq;
+
+namespace csqlite
+{
+    static class DBHelper //extensions
+    {
+        public static List<Product> GetProductList(this Db db)
+        {
+            return db.GetList<Product>("select * from PRODUCT", Product.Create);
+        }
+
+        //sql
+        public static Product GetProductByNameSql(this Db db, string name)
+        {
+            return db.GetList<Product>($"select * from PRODUCT where NAME like '{name}%' limit 1", Product.Create).FirstOrDefault();
+        }
+
+        //linq
+        public static Product GetProductByNameLinq(this Db db, string name)
+        {
+            return db.GetProductList().Where(x => x.Name == name).FirstOrDefault();
+        }
+
+        //sql
+        public static List<Product> GetProductListWithPriceSql(this Db db, double price)
+        {
+            return db.GetList<Product>($"select * from PRODUCT where PRICE like '{price}%'", Product.Create).ToList();
+        }
+
+        //linq
+        public static List<Product> GetProductListWithPriceLinq(this Db db, double price)
+        {
+            return db.GetProductList().Where(x => x.Price == price).ToList();
+        }
+    }
+}
