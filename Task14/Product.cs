f176ad4014b361e6dbeab455c3b0ad02963d88c6
Владислав Май <91732525+DeLoreannn@users.noreply.github.com>
2022-01-06 20:15:46 +0200

    Add files via upload

---
+++ Task14/Product.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using Microsoft.Data.Sqlite;
+using System.Linq;
+
+namespace csqlite
+{
+    class Product
+    {
+        private double price;
+        private double weight;
+        public string Name { get; set; }
+        public double Price
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                try
+                {
+                    if (value <= 0)
+                        throw new ArgumentException("Incorrect value of price");
+                    price = value;
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+        public double Weight
+        {
+            get
+            {
+                return weight;
+            }
+            set
+            {
+                try
+                {
+                    if (value <= 0)
+                        throw new ArgumentException("Incorrect value of weight");
+                    weight = value;
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        public Product() { }
+        public Product(string Name, double Price, double Weight)
+        {
+            this.Name = Name;
+            this.Price = Price;
+            this.Weight = Weight;
+        }
+        public override string ToString()
+        {
+            return $"Name: {Name}, price: {Price} grn, weight: {Weight} g";
+        }
+        public static Product Create(IDataRecord data)
+        {
+            return new Product(data["NAME"].ToString(), Convert.ToDouble(data["PRICE"].ToString()), Convert.ToDouble(data["WEIGHT"].ToString()));
+        }
+    }
+}
---
+++ Task14/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using Microsoft.Data.Sqlite;
+using System.Linq;
+
+
+namespace csqlite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Db db = new Db("Data Source=data.db");
+
+            foreach (var product in db.GetProductList())
+            {
+                Console.WriteLine(product);
+            }
+
+            Console.WriteLine(db.GetProductByNameSql("Bounty"));
+
+            foreach (var product in db.GetProductListWithPriceLinq(30))
+            {
+                Console.WriteLine(product);
+            }
+        }
+    }
+}
