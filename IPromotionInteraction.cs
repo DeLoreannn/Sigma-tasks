d17a3328b8c0d7f78366f03d7314b56fadaf0889
Владислав Май <91732525+DeLoreannn@users.noreply.github.com>
2022-01-09 17:45:33 +0200

    Delete IPromotionInteraction.cs

--- IPromotionInteraction.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Shop_cousework.Interfaces
-{
-    interface IPromotionInteraction
-    {
-        void AddGoodsToPromotion(Type type, Goods goods, int percentage);
-
-        void RemoveGoodsFromPromotion(Type type, Goods goods);
-    }
-}
